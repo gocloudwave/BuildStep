9032aca4e693e676c1d50278bfe33051e569ba7c
benjaminsanderssensato <bsanders@gocloudwave.com>
2025-10-15 15:55:28 -0600

    Update pull_build_repo.sh

--- pull_build_repo.sh
+++ pull_build_repo.sh
@@ -11,4 +11,32 @@ sudo apt install -y git
 
 cd /opt
 
-sudo git clone https://$USER:$TOKEN@github.com/CloudWave-CaaS/SentinelBuild.git
+echo "Select an option to clone:"
+echo "1) Clone SentinelBuildScripts"
+echo "2) Clone HoneypotBuildScripts"
+echo "3) Clone VScanScripts"
+echo "9) Exit"
+read -p "Enter your choice [1-3, 9]: " choice
+
+case $choice in
+    1)
+        sudo git clone https://$USER:$TOKEN@github.com/gocloudwave/SentinelBuildScripts.git
+        echo "Cloned SentinelBuildScripts"
+        ;;
+    2)
+        sudo git clone https://$USER:$TOKEN@github.com/gocloudwave/HoneypotBuildScripts.git
+        echo "Cloned HoneypotBuildScripts"
+        ;;
+    3)
+		sudo git clone https://$USER:$TOKEN@github.com/gocloudwave/VScanBuildScripts.git
+        echo "Cloned both repositories"
+        ;;
+    9)
+        echo "Exiting..."
+        exit 0
+        ;;
+    *)
+        echo "Invalid choice. Exiting..."
+        exit 1
+        ;;
+esac
